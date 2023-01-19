b2d51b3483326dc9bb9ca903f5d2ef3032984ae2
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-01-19 18:44:10 +0100

    Updates Testnet smarrt contract

--- renderer/blockchain.js
+++ renderer/blockchain.js
@@ -4,7 +4,7 @@ const {ipcRenderer} = require("electron");
 let EticaContractJSON = require('../EticaRelease.json');
 
 //const ETICA_ADDRESS = '0x34c61EA91bAcdA647269d4e310A86b875c09946f'; // mainnet
-const ETICA_ADDRESS = '0xE5817921cc87FBd26e160AD1114bCb29cE248679'; // local dev blockchain
+const ETICA_ADDRESS = '0x22Bbf0fddb3c005a1E5e263Aa71A65Eb6146E9CE'; // local dev blockchain
 
 class Blockchain {
   
