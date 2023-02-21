df170061103b1f0b2ff322f912a9f04771115843
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-02-21 16:52:47 +0100

    Seed generation in process

--- renderer/seed.js
+++ renderer/seed.js
@@ -1,5 +1,7 @@
 const {ipcRenderer} = require("electron");
 const bip39 = require("bip39");
+const { hdkey } = require('ethereumjs-wallet');
+const util = require('ethereumjs-util');
 
 
  $("#GenerateMnemonic").off("click").on("click", function () {
@@ -15,13 +17,31 @@ const bip39 = require("bip39");
   console.log('type of seed is', typeof seed);
   console.log('Generated seed:', seed.toString('hex'));
 
+
+// Generate a key pair from the seed
+const hdWallet = hdkey.fromMasterSeed(seed);
+const wallet = hdWallet.derivePath("m/44'/60'/0'/0/0").getWallet();
+const privateKey = wallet.getPrivateKey();
+const publicKey = wallet.getPublicKey();
+
+console.log('Private key:', privateKey.toString('hex'));
+console.log('Public key:', publicKey.toString('hex'));
+
+// Calculate the Ethereum address from the public key
+let address_without0x = util.pubToAddress(publicKey, true).toString('hex');
+const address = '0x' + address_without0x;
+
+console.log('Ethereum address:', '0x' + address);
+
+
   $("#GenerateMnemonicDiv").hide();
-  $("#HelperMnemonic").html("This is your seed.");
+  $("#HelperMnemonic").html("This is your seed");
   $("#NewMnemonic").css('display', 'block');
   $("#NewMnemonic").html(mnemonic);
   $("#GoCheckMnemonicDiv").css('display', 'block');
 
-  console.log('Generated mnemonic:', mnemonic);
+  $("#NewEticaAddress").css('display', 'block');
+  $("#NewEticaAddress").html('Etica address: '+address+'');
 
 
   });
