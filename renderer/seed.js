6933636ed2a82cd189c715cf853512de82cb5ac4
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-02-22 05:46:50 +0100

    Seed generation in process, User Seed verification and validation done

--- renderer/seed.js
+++ renderer/seed.js
@@ -3,11 +3,18 @@ const bip39 = require("bip39");
 const { hdkey } = require('ethereumjs-wallet');
 const util = require('ethereumjs-util');
 
+let mnemonic;
+let user_mnemonic_order_array = [];
 
  $("#GenerateMnemonic").off("click").on("click", function () {
 
+  // reinitialize mnemonic
+  mnemonic = '';
+  user_mnemonic_order_array = [];
+
+
   const strength = 256; // strength in bits, must be a multiple of 32
-  const mnemonic = bip39.generateMnemonic(strength);
+  mnemonic = bip39.generateMnemonic(strength);
 
   console.log('Generated mnemonic:', mnemonic);
 
@@ -45,3 +52,84 @@ console.log('Ethereum address:', '0x' + address);
 
 
   });
+
+
+  $("#GoCheckMnemonic").off("click").on("click", function () {
+
+    $("#NewMnemonic").css('display', 'none');
+    $("#GoCheckMnemonicDiv").css('display', 'none');
+    $("#NewEticaAddress").css('display', 'none');
+    $("#CheckMnemonic").css('display', 'block');
+
+    $("#HelperMnemonic").html("Select words in right order");
+
+    // extract words:
+
+    const initialWords = mnemonic.split(' ');
+    console.log('initialWords is', initialWords);
+
+
+// Shuffle the indices using the Fisher-Yates shuffle algorithm
+const indices = Array.from(Array(24).keys());
+for (let i = indices.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [indices[i], indices[j]] = [indices[j], indices[i]];
+}
+
+// Rearrange the words according to the shuffled indices
+const reorderedWords = indices.map(index => initialWords[index]);
+$("#word1").html(reorderedWords[0]);
+$("#word2").html(reorderedWords[1]);
+$("#word3").html(reorderedWords[2]);
+$("#word4").html(reorderedWords[3]);
+$("#word5").html(reorderedWords[4]);
+$("#word6").html(reorderedWords[5]);
+$("#word7").html(reorderedWords[6]);
+$("#word8").html(reorderedWords[7]);
+$("#word9").html(reorderedWords[8]);
+$("#word10").html(reorderedWords[9]);
+$("#word11").html(reorderedWords[10]);
+$("#word12").html(reorderedWords[11]);
+$("#word13").html(reorderedWords[12]);
+$("#word14").html(reorderedWords[13]);
+$("#word15").html(reorderedWords[14]);
+$("#word16").html(reorderedWords[15]);
+$("#word17").html(reorderedWords[16]);
+$("#word18").html(reorderedWords[17]);
+$("#word19").html(reorderedWords[18]);
+$("#word20").html(reorderedWords[19]);
+$("#word21").html(reorderedWords[20]);
+$("#word22").html(reorderedWords[21]);
+$("#word23").html(reorderedWords[22]);
+$("#word24").html(reorderedWords[23]);
+
+
+  });
+
+
+  $(".mnemonicwords").off("click").on("click", function () {
+    user_mnemonic_order_array.push($(this).html());
+    $(this).hide();
+    console.log('user_mnemonic_order_array is', user_mnemonic_order_array);
+
+    if(user_mnemonic_order_array.length == 24){
+      // check mnemonic:
+      let usermnemonic = user_mnemonic_order_array.join(' ');
+
+      console.log('usermnemonic is: ', usermnemonic);
+
+      if(normalizeString(mnemonic) === normalizeString(usermnemonic)){
+console.log('validmnemonic');
+      }
+      else {
+        console.log('invalidmnemonic');
+      }
+
+
+    }
+
+  });
+
+  function normalizeString(str) {
+    return str.trim().replace(/\s+/g, ' ');
+  }
