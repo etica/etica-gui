31258fe4c996b3a5ec0b7799137fef91047ed37f
Kevin Wad <k.w.x.m.r@protonmail.com>
2023-03-05 15:10:07 +0100

    Implements put elctron-storage default dir in wallet.datadir

--- renderer/database.js
+++ renderer/database.js
@@ -1,11 +1,18 @@
 // In renderer process (web page).
 const {ipcRenderer} = require("electron");
 
-class Datatabse {
-  constructor() {}
+class Datatabase {
+  constructor() {
+
+
+  }
 
   getCounters() {
-    var counters = ipcRenderer.sendSync("getJSONFile", "counters.json");
+    let runningwallet = ipcRenderer.sendSync("getRunningWallet");
+    let dbWalletDataDirectory = runningwallet.datadirectory;
+    let _filepath = dbWalletDataDirectory+'/counters.json';
+    console.log('getCounters _filepath is', _filepath);
+    var counters = ipcRenderer.sendSync("getJSONFile", _filepath);
 
     if (counters == null) {
       counters = {};
@@ -15,14 +22,22 @@ class Datatabse {
   }
 
   setCounters(counters) {
+    let runningwallet = ipcRenderer.sendSync("getRunningWallet");
+    let dbWalletDataDirectory = runningwallet.datadirectory;
+    let _filepath = dbWalletDataDirectory+'/counters.json';
+    console.log('setCounters _filepath is', _filepath);
     ipcRenderer.sendSync("setJSONFile", {
-      file: "counters.json",
+      file: _filepath,
       data: counters
     });
   }
 
   getAddressesNames() {
-    var addressesnames = ipcRenderer.sendSync("getJSONFile", "wallets.json");
+    let runningwallet = ipcRenderer.sendSync("getRunningWallet");
+    let dbWalletDataDirectory = runningwallet.datadirectory;
+    let _filepath = dbWalletDataDirectory+'/wallets.json';
+    console.log('getAddressesNames _filepath is', _filepath);
+    var addressesnames = ipcRenderer.sendSync("getJSONFile", _filepath);
 
     if (!addressesnames) {
       addressesnames = {
@@ -34,14 +49,22 @@ class Datatabse {
   }
 
   setAddressesNames(addressesnames) {
+    let runningwallet = ipcRenderer.sendSync("getRunningWallet");
+    let dbWalletDataDirectory = runningwallet.datadirectory;
+    let _filepath = dbWalletDataDirectory+'/wallets.json';
+    console.log('setAddressesNames _filepath is', _filepath);
     ipcRenderer.sendSync("setJSONFile", {
-      file: "wallets.json",
+      file: _filepath,
       data: addressesnames
     });
   }
 
   getAddresses() {
-    var addressBook = ipcRenderer.sendSync("getJSONFile", "addresses.json");
+    let runningwallet = ipcRenderer.sendSync("getRunningWallet");
+    let dbWalletDataDirectory = runningwallet.datadirectory;
+    let _filepath = dbWalletDataDirectory+'/addresses.json';
+    console.log('getAddresses _filepath is', _filepath);
+    var addressBook = ipcRenderer.sendSync("getJSONFile", _filepath);
 
     if (!addressBook) {
       addressBook = {
@@ -53,6 +76,10 @@ class Datatabse {
   }
 
   setAddresses(addresses) {
+    let runningwallet = ipcRenderer.sendSync("getRunningWallet");
+    let dbWalletDataDirectory = runningwallet.datadirectory;
+    let _filepath = dbWalletDataDirectory+'/addresses.json';
+    console.log('setAddresses _filepath is', _filepath);
     ipcRenderer.sendSync("setJSONFile", {
       file: "addresses.json",
       data: addresses
@@ -60,5 +87,7 @@ class Datatabse {
   }
 }
 
+
+
 // create new account variable
-EticaDatabase = new Datatabse();
+EticaDatabase = new Datatabase();
